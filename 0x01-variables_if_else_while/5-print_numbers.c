7a5bc491a03f7abfb7be2117e446f51d970ab9d0
Demmydee <demmydd44@gmail.com>
2022-12-09 14:57:47 -0800

    print o to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry point
+ *
+ * Return: must zero
+ */
+
+int main(void)
+{
+	int c;
+
+	for (c = 0; c <= 9; c++)
+	{
+		printf("%d\n", c);
+	}
+	return (0);
+}
