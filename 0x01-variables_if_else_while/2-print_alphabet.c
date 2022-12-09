0b859d7314aca63dfb1ffd08784437abc479f769
Demmydee <demmydd44@gmail.com>
2022-12-09 10:28:55 -0800

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: must zero
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c == 'a', c < 'z', ++c)
+	{
+		putchar("%c", c);
+	}
+	return (0);
+}
