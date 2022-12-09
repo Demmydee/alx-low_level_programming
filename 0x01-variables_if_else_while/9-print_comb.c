f1b62ca8884de004cbf1e9a68f831e8a72b51a1a
Demmydee <demmydd44@gmail.com>
2022-12-09 15:42:09 -0800

    print comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -13,9 +13,10 @@ int main(void)
 	for (c = '0'; c <= '9'; c++)
 	{
 		putchar(c);
-		putchar(' ');
 		if (c < 9)
-		putchar(', ');
+		{
+		putchar(',');
+		putchar(' ');
 	}
 		putchar('\n');
 	return (0);
