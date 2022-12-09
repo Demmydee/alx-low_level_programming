c3eec82fdc029253e042862cbc45ed2d5b1a9061
Demmydee <demmydd44@gmail.com>
2022-12-09 14:37:02 -0800

    print all alphabets but q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry point
+ *
+ * Return: must zero
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if !((c == q) || (c == e))
+		putchar(c);
+	}
+		putchar('\n');
+	return (0);
+}
