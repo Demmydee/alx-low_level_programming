1e34f691bcd2864b3283856e8f2afea99b47c716
Demmydee <demmydd44@gmail.com>
2022-12-09 15:03:08 -0800

    print numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry point
+ *
+ * Return: must zero
+ */
+
+int main(void)
+{
+	int c;
+
+	for (c = 0; c <= 9; c++)
+	{
+		putchar("%d", c);
+	}
+		putchar('\n');
+	return (0);
+}
