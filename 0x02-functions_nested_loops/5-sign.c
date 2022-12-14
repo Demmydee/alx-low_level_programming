e8813af4a4978123040396cb08d4b8b7f558de0d
Demmydee <demmydd44@gmail.com>
2022-12-13 17:08:11 -0800

    signs

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,14 +12,14 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		x = 1;
-		_putchar('+');
+		_putchar('+1');
 	}
 	else if (n == 0)
 	{
 		x = 0;
 		_putchar('0');
 	}
-	else (n == 0)
+	else (n < 0)
 	{
 		x = -1;
 		_putchar('-1');
