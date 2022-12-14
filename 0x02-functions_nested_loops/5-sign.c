a9a735c4e580a64612b4660586310a56452cd88d
Demmydee <demmydd44@gmail.com>
2022-12-13 17:04:55 -0800

    signs

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_sign - entry point
+ * @n: input
+ * Return: 1 if number is positive, 0 if it is zero & -1 if negative
+ */
+int print_sign(int n)
+{
+	int x;
+
+	if (n > 0)
+	{
+		x = 1;
+		_putchar('+');
+	}
+	else if (n == 0)
+	{
+		x = 0;
+		_putchar('0');
+	}
+	else (n == 0)
+	{
+		x = -1;
+		_putchar('-1');
+	}
+	return (x);
+}
