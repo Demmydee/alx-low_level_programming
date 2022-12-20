ca17dbc0e45e1b1e8e2e5d627b37cc56a2dc8b57
Demmydee <demmydd44@gmail.com>
2022-12-19 18:53:25 -0800

    print half of a string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -11,9 +11,9 @@ void puts_half(char *str)
 	while (str[len] != '\0')
 		len++;
 	if (len % 2 == 0)
-		n = len / 2;
-	else n = (len + 1) / 2;
-	for (i = n; i < len; i++)
+		j = len / 2;
+	else j = (len + 1) / 2;
+	for (i = j; i < len; i++)
 		putchar(str[i]);
 	putchar('\n');
 }
