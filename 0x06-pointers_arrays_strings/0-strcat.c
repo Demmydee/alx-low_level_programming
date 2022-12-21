9c6963d4fa3d0b386518fcbee8613d64f4299956
Demmydee <demmydd44@gmail.com>
2022-12-21 15:22:31 -0800

    concatenate two strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _strcat - concatenates two strings
+ * @dest: deatination string
+ * @src: source string
+ * Return: dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	strcat(dest, src);
+	return (dest);
+}
