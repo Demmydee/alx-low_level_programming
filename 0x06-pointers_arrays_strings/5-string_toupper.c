811285676babd0b6d13abd7bc60992d907f53869
Demmydee <demmydd44@gmail.com>
2022-12-22 18:10:51 -0800

    change lowercase to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * string_toupper - changes all lowercase to uppercase
+ * Return: character
+ */
+char *string_toupper(char *)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] -= 32;
+	}
+	return (str);
+}
