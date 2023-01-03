793276cf6f9c29e08086c0b09d3b7ec7f59eeef2
Demmydee <demmydd44@gmail.com>
2023-01-02 17:00:03 -0800

    locates a substring

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -3,14 +3,16 @@
  * _strstr - locates a substring
  * @haystack: string to check substring from
  * @needle: substring to be checked
- * Return: A pointer to the beginning of substring or null if no substring found
+ * Return: A pointer to the beginning of substring
+ * or null if no substring found
  */
-char *_strchr(char *haystack, char *needle)
+char *_strstr(char *haystack, char *needle)
 {
 	for (; *haystack != '\0'; haystack++)
 	{
 		char *h = haystack;
 		char *n = needle;
+
 		while (*h == *n && *n != '\0')
 		{
 			h++;
