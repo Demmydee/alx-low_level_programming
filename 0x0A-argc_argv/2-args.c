d4340e2e91ee96b37a0e91028094d3cc6389dbca
Demmydee <demmydd44@gmail.com>
2023-01-06 12:46:03 -0800

    return arguments

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - kndkn v
+ * @argc: okomjnj
+ * @argv: kmkfvmk
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	while(argc--)
+		printf("%s\n", *argv++);
+	return (0);
+}
