b6a5b49ec770f23efc0ae0657b2e84c91fe69e9a
Demmydee <demmydd44@gmail.com>
2023-01-06 15:38:57 -0800

    print the number of arguments

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -6,10 +6,10 @@
  * @argv: array of arguments
  * Return: 0
  */
-int main(int argc, char **argv)
+int main(int argc, char *argv[]__attribute__((unused)))
 {
-	(void) argv;
+	if (argc > 0)
+		printf("%d\n", argc - 1);
 
-	printf("%d\n", argc);
 	return (0);
 }
