20221e5883e30f26ad2e732b4b423bfdf6d19f47
Demmydee <demmydd44@gmail.com>
2023-01-06 16:45:55 -0800

    multiply 2 numbers

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,6 +1,36 @@
 #include "main.h"
 #include <stdio.h>
 /**
+ * _atoi - converts a string to an integer
+ * @s: supplied string
+ * Return: 0
+ */
+
+int _atoi(char *s)
+{
+	int sign = 1;
+	unsigned int total = 0;
+	char null_flag = 0;
+
+	while (*s)
+	{
+		if (*s == '-')
+			sign *= -1;
+
+		if (*s >= '0' && *s <= '9')
+		{
+			null_flag = 1;
+			total = total * 10 + *s - '0';
+		}
+		else if (null_flag)
+			break;
+		s++;
+	}
+	if (sign < 0)
+		total = (-total);
+	return (total);
+}
+/**
  * main - prints the name of the program
  * @argc: number of arguments
  * @argv: array of arguments
@@ -18,7 +48,7 @@ int main(int argc, char **argv)
 	}
 	for (i = 1; i < 3; i++)
 	{
-		res = res * atoi(argv[i]);
+		res = res * _atoi(argv[i]);
 	}
 	printf("%d\n", res);
 
