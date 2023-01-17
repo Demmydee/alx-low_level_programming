163b56a13c15aa12a2480f1c6c7323ec64ce8d15
Demmydee <demmydd44@gmail.com>
2023-01-17 09:20:29 -0800

    print struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -6,18 +6,24 @@
  * @name: is the name element
  * @age: is the age element
  * @owner: is the owner element
+ * Description: longer description
  */
 
-typedef struct dog dog_t
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+}
+/**
+ * dog_t - typedef fpr struct dog
+ */
 typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
 
 #endif
