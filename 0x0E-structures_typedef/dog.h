b8ea9088e4f6fc8732971757bf380ab6b5dd3293
Demmydee <demmydd44@gmail.com>
2023-01-17 09:52:39 -0800

    struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -20,6 +20,12 @@ struct dog
  * dog_t - typedef fpr struct dog
  */
 
+typedef struct dog dog_t;
+{
+	char *name;
+	float age;
+	char *owner;
+};
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
