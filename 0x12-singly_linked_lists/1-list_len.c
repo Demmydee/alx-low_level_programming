aec4f9e9c6918e4d27829091958dbffd600d2610
Demmydee <demmydd44@gmail.com>
2023-01-26 05:11:30 -0800

    return the number of elements in a linked list_t list

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -3,15 +3,10 @@
 
 
 /**
-
  * list_len - returns the number of elements in a linked list_t list.
-
  * @h: the list
-
  *
-
  * Return: the number of elements in a linked list_t list.
-
  */
 
 size_t list_len(const list_t *h)
