698ad631bcf4edbd50e917e2ec6096939ec96b44
Olaoye Ademola <demmydd44@gmail.com>
2024-03-21 09:06:58 -0700

    return the number of elements in alinked list

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - return the number of elements in
+ * a linked list
+ *
+ * @h: head of the list
+ * Return: the number of nodes
+ */
+
+size_t dlistin_len(const dlistint_t *h)
+{
+	int element = 0;
+
+	if (h == NULL)
+		return (element);
+
+	while (h->prev != NULL)
+		h = h->prev;
+
+	while (h != NULL)
+	{
+		element++;
+		h = h->next;
+	}
+
+	return (element);
+}
