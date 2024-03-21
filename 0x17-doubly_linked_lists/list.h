c520e4d8582d0efe6cfe07ce4b329cb51b1632cb
Olaoye Ademola <demmydd44@gmail.com>
2024-03-21 08:57:37 -0700

    print all the elements of a list

--- 0x17-doubly_linked_lists/list.h
+++ 0x17-doubly_linked_lists/list.h
@@ -2,7 +2,6 @@
 #define _LISTS_
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
  * struct dlistint_s - doubly linked list
  * @n: integer
@@ -10,13 +9,13 @@
  * @next: points to the next node
  *
  * Description: doubly linked list node structure
- * 
+ *
  */
 typedef struct dlistint_s
 {
-    int n;
-    struct dlistint_s *prev;
-    struct dlistint_s *next;
+	int n;
+	struct dlistint_s *prev;
+	struct dlistint_s *next;
 } dlistint_t;
 
 size_t print_dlistint(const dlistint_t *h);
